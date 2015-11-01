91619d976e233e7c537786c583f4ac54760d1614
David.H <david.hu@getperx.com>
2015-11-01 21:21:54 +0800

    Add content

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
+var Band = Ember.Object.extend({
+  name: '',
+});
+
+var ledZeppelin = Band.create({ name: 'Led Zeppelin' });
+var pearlJam = Band.create({ name: 'Pearl Jam' });
+var fooFighters = Band.create({ name: 'Foo Fighters' });
+var u2 = Band.create({ name: 'U2' });
+
+var bands = [ledZeppelin, pearlJam, fooFighters, u2];
+
 export default Ember.Route.extend({
+  model() {
+    return bands;
+  }
 });
