1017899a27c4d4f5f8bc725a29ed729c00b9e439
David.H <david.hu@getperx.com>
2015-11-02 10:19:26 +0800

    Create song

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songCreationStarted: false,
+  
+  noSongs: Ember.computed('model.songs.length', function() {
+    return this.get('model.songs.length') === 0;
+  }),
+
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
+
   actions: {
+    enableSongCreation() {
+      this.set('songCreationStarted', true);
+    },
+
     updateRating(params){
       var song = params.item,
           rating = params.rating;
